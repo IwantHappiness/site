87ac6ce3844087afcffcb0a1af6c7dabcbdf69f2
IwantHappiness <iwanthappiness95@gmail.com>
2025-05-24 13:20:59 +0300

    update(lib): rust dependen

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,18 @@ use std::env;
 
 #[get("/")]
 async fn index() -> impl Responder {
-	NamedFile::open_async("./dist/index.html").await
+    NamedFile::open_async("./dist/index.html").await
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-	let port = env::var("PORT")
-		.unwrap_or_else(|_| "8080".to_string())
-		.parse::<u16>()
-		.unwrap();
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| "8080".to_string())
+        .parse::<u16>()
+        .unwrap();
 
-	HttpServer::new(|| {
-		App::new().service(index).service(Files::new("/", "./dist"))
-	})
-	.bind(("0.0.0.0", port))?
-	.run()
-	.await
+    HttpServer::new(|| App::new().service(index).service(Files::new("/", "./dist")))
+        .bind(("0.0.0.0", port))?
+        .run()
+        .await
 }
